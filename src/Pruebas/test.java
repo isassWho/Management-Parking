40e2461392fe11ad56369bed09d0c16e90371f72
Isai Lugo <isass_who@hotmail.com>
2019-11-30 14:57:13 -0600

    Vistas Funcionales v1

---
+++ src/Pruebas/test.java
@@ -0,0 +1,24 @@
+
+package Pruebas;
+
+import Vista.Login;
+
+/**
+ *
+ * @author Isass
+ */
+
+public class test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        Login login = new Login();
+        login.setVisible(true);
+        
+
+    }
+
+}
